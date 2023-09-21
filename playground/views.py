34999164e28cfe0cd7ad99219a88544aa934132e
Achraf333-m <achrafdaimallah2003@gmail.com>
2023-09-21 00:36:04 -0400

    debugger practice

--- playground/views.py
+++ playground/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def calculate ():
+    x = 1
+    y = 2
+    return x + y
+
 def say_hello (request):
-    return render(request, 'hello.html', {"name" : "Achraf"})
+    x = calculate()
+    return render(request, 'test.html', {"name" : "Achraf"})
